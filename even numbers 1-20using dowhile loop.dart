07b9625ebafe06e086755a8721f2b80d3599a361
judsonwilliam18 <judsonwills18@gmail.com>
2026-04-15 17:00:54 +0530

    first commit by judson

---
+++ even numbers 1-20using dowhile loop.dart
@@ -0,0 +1,10 @@
+void main(){
+  var i=1;
+  do{
+    if(i%2==0){
+    print(i);
+    } 
+    i++;
+  } while(i<=20);
+  
+}
---
+++ operators.dart
@@ -0,0 +1,25 @@
+void main(){
+  int a=5;
+  int b=7;
+  
+  //Arithmetic operators
+  print("arithmetic operators");
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a%b);
+  //Assignment operators
+  print("Assignment operators");
+  print(a==b);
+  print(a!=b);
+  print(a<b);
+  print(a>b);
+  print(a<=b);
+  print(a>=b);
+  //logical operators
+  print("logical operators");
+  print(a&b);
+  print(a|b);
+}
+  
+  
