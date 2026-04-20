458d5ab247850c31367532e89cffcc5553009a96
judsonwilliam18 <judsonwills18@gmail.com>
2026-04-20 16:41:58 +0530

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(MyflutterApp());
-class MyflutterApp   extends StatelessWidget {
-  
-
-  @override
+import 'dart:math';
+void main()=>runApp(CoinFlip());
+class CoinFlip extends StatelessWidget {
+  int CoinHead=0;
+Random coinside=new Random();
+   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(title: Text("MyApp"),),
-      body: material(
-        Color:Colors.green
-        child:Center(
-          child: Text(
-            "MyApp",
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text(
+            'The coin is :'+(CoinHead==1?'head':'tail'),
           ),
-        )
-
+        ),
+        floatingActionButton: FloatingActionButton(
+         child: Icon(Icons.attach_money),
+          onPressed: () {
+            CoinHead=coinside.nextInt(2);
+            print(CoinHead);
+          },
+        ),
       ),
-      
-
-
-    ),
-   );
+    );
   }
 }
