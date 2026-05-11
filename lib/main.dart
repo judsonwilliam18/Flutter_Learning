bd85f1cb22954167d3ee08f884bbffb27394d1b2
judsonwilliam18 <judsonwills18@gmail.com>
2026-05-11 16:04:27 +0530

    project

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,103 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-void main()=>runApp(CoinFlip());
-class CoinFlip extends StatelessWidget {
-  int CoinHead=0;
-Random coinside=new Random();
-   @override
+void main()=>runApp(MyApp());
+class MyApp extends   StatelessWidget {
+    @override
   Widget build(BuildContext context) {
-    //elmegelg;,e,g
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title:Text(
-            'The coin is :'+(CoinHead==1?'head':'tail'),
+        backgroundColor: Colors.blueGrey,
+        body: Column(
+          //judson judson 
+          children: <Widget>[
+            Expanded(
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 50,width: 30,child: Image.asset('assets/left hand.jpg'),),
+                  SizedBox(height: 50,width: 30,child: Image.asset('assets/music.jpg'),)
+                ],
+              ) ),
+              Expanded(
+                child:Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Hey! ",style: TextStyle(color: Colors.white,fontSize: 40.0),),
+                    Text("Welcome Judson",style: TextStyle(color: Colors.white,fontSize: 40.0),)
+                  ],
+                ) ),
+                Expanded(
+                  child:Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Email  or  Username")
+                    ],
+                  ) ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Password")
+                      ],
+                    )),
+                    Expanded(
+                      child:Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+
+                        children: [
+                          TextButton(onPressed: (){
+
+                          }, child: Text("Forget password?"))
+                        ],
+                      ) ),
+                      Expanded(
+                        child:Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(onPressed: (){
+
+                            }, child: Text("LOG IN >",style: TextStyle(color: Colors.white,fontSize: 20.0)))
+                          ],
+                        ) ),
+                        Expanded(
+                          child:Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("or continue with")
+                            ],
+                          ) ),
+                          Expanded(
+                            child:Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              
+                              children: [
+                                SizedBox(height: 50,width: 30,child: Image.asset('assets/google logo.jpg'),),
+                                SizedBox(height: 50,width: 30,child: Image.asset('assets/facebook logo.jpg'),)
+                              ],
+                            ) ),
+                            Expanded(
+                              child:Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text("Don't have an account?",style: TextStyle(color: Colors.white),),
+                                  TextButton(onPressed: (){
+
+                                  }, child: Text("LOG IN",style: TextStyle(color: Colors.blue,fontSize: 30.0),))
+                                ],
+                              ) )
+
+               
+          ],
+          
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-         child: Icon(Icons.attach_money),
-          onPressed: () {
-            CoinHead=coinside.nextInt(2);
-            print(CoinHead);
-          },
-        ),
+           
       ),
     );
   }
