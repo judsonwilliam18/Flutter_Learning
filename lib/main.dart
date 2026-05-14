4e497d5f164c23fb4e0270f7ca2d5aa0b6c1d7d6
judsonwilliam18 <judsonwills18@gmail.com>
2026-05-14 17:50:50 +0530

    Json

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,67 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-void main()=>runApp(MyApp());
-class MyApp extends   StatelessWidget {
-    @override
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // JSON String
+  final String jsonString = '''
+  { 
+    "ust global employee": [ 
+      {
+        "name": "judson",
+        "Age": 24,
+        "Salary": 24000
+      },
+      {
+        "name": "David",
+        "Age": 25,
+        "Salary": 27000
+      }
+    ]
+  }
+  ''';
+  
+
+  @override
   Widget build(BuildContext context) {
+    // Convert JSON string to Map
+    final data = jsonDecode(jsonString);
+    final encodedata = jsonEncode(jsonString);
+    final decodeddata = jsonDecode(jsonString);
+
+
+    debugPrint("encodedata: ====${encodedata}");
+    debugPrint("decodedata: ====${decodeddata}");
+
+
+    // Get employee list
+    List employees = data["ust global employee"];
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Column(
-          //judson judson 
-          children: <Widget>[
-            Expanded(
-            //  oljuojoijj//
-              child:Row(
-                
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 50,width: 30,child: Image.asset('assets/left hand.jpg'),),
-                  SizedBox(height: 50,width: 30,child: Image.asset('assets/music.jpg'),)
-                ],
-              ) ),
-              Expanded(
-                child:Row(
-                  
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Hey! ",style: TextStyle(color: Colors.white,fontSize: 40.0),),
-                    Text("Welcome Judson",style: TextStyle(color: Colors.white,fontSize: 40.0),)
-                  ],
-                ) ),
-                Expanded(
-                  child:Row(
-                    
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Email  or  Username")
-                    ],
-                  ) ),
-                  Expanded(
-                    child: Row(
-                      
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Password")
-                      ],
-                    )),
-                    Expanded(
-                      child:Row(
-                        
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-
-                        children: [
-                          TextButton(onPressed: (){
-
-                          }, child: Text("Forget password?"))
-                        ],
-                      ) ),
-                      Expanded(
-                        child:Row(
-                          
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextButton(onPressed: (){
-
-                            }, child: Text("LOG IN >",style: TextStyle(color: Colors.white,fontSize: 20.0)))
-                          ],
-                        ) ),
-                        Expanded(
-                          child:Row(
-                            
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text("or continue with")
-                            ],
-                          ) ),
-                          Expanded(
-                            child:Row(
-                              
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              
-                              children: [
-                                SizedBox(height: 50,width: 30,child: Image.asset('assets/google logo.jpg'),),
-                                SizedBox(height: 50,width: 30,child: Image.asset('assets/facebook logo.jpg'),)
-                              ],
-                            ) ),
-                            Expanded(
-                              child:Row(
-                                
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text("Don't have an account?",style: TextStyle(color: Colors.white),),
-                                  TextButton(onPressed: (){
-
-                                  }, child: Text("LOG IN",style: TextStyle(color: Colors.blue,fontSize: 30.0),))
-                                ],
-                              ) )
-
-               
-          ],
-          
-          ),
-           
+        appBar: AppBar(
+          title: Text("Employee Data"),
+        ),
+        body: ListView.builder(
+          itemCount: employees.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(
+                  employees[index]["name"],
+                ),
+                subtitle: Text(
+                  "Age: ${employees[index]["Age"]}\n"
+                  "Salary: ${employees[index]["Salary"]}",
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
