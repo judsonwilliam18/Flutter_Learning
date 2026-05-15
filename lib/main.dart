c741ecb07e04e17e2894bdad51257cdcdcdc0945
judsonwilliam18 <judsonwills18@gmail.com>
2026-05-15 16:09:25 +0530

    shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,76 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-
-  // JSON String
-  final String jsonString = '''
-{
-  "ust global employee": [
-    {
-      "name": "judson",
-      "Age": 24,
-      "Salary": 24000
-    },
-    {
-      "name": "David",
-      "Age": 25,
-      "Salary": 27000
-    }
-  ]
-}
-  ''';
+void main()=>runApp(localstorage());
+class localstorage extends  StatefulWidget {
+  const localstorage({super.key});
 
   @override
+  State<localstorage> createState() => _localstorageState();
+}
+
+class _localstorageState extends State<localstorage> {
+   String localstorage='flutter';
+  @override
   Widget build(BuildContext context) {
-
-    // Decode JSON
-    Map<String, dynamic> data = jsonDecode(jsonString);
-
-    // Employee List
-    List employees = data["ust global employee"];
-
-    // Company List
-    List companies = data["ust global company"] ??[];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("JSON Display"),
+          backgroundColor: Colors.green,
+          title: Text("Localstorage"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(" $localstorage"),
+            ElevatedButton(onPressed: (){
+              writedata();
+              print('success');
+            },
+             child: const Text("write data")),
+             ElevatedButton(onPressed: (){
+              readdata();
+             }, child: const Text("Read data")),
+              ElevatedButton(onPressed: (){
+                updatedata();
+              }, child: const Text("update data")),
+               ElevatedButton(onPressed: (){
+                deletedata();
+               }, child: const Text("delete data"))
+
+
+
+          ],
         ),
 
-        body: Padding(
-          padding: EdgeInsets.all(12),
-
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Text(
-                  "Employees",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                SizedBox(height: 10),
-
-                // Employee Data
-                for (var emp in employees)
-                  Card(
-                    
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Name : ${emp['name']}"),
-                          Text("Age : ${emp['Age']}"),
-                          Text("Salary : ${emp['Salary']}"),
-                        ],
-                      ),
-                    
-                  ),
-
-                
-
-                Text(
-                  "Companies",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-              
-
-                // Company Data
-                for (var company in companies)
-                  Card(
-                    
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Country : ${company['country']}"),
-                          Text("Employees : ${company['employees']}"),
-                        ],
-                      ),
-                    
-                  ),
-              ],
-            ),
-          ),
-        ),
       ),
     );
+  }
+  void writedata()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('name', 'logo tech');
+
+
+  }
+  void readdata()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? action = prefs.getString('name');
+    setState(() {
+      localstorage=action!;
+    });
+  }
+  void updatedata()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('name', 'logo info tech');
+  }
+  void deletedata()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('name');
+    setState(() {
+      localstorage='flutter';
+    });
+
   }
 }
